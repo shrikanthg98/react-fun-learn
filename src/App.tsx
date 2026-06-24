3a6c7b49a1ac94120bda4dbb158108c204db3f97
Shrikanth G <shrikanthg18@gmail.com>
2026-06-24 22:38:13 +0530

    minor style changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ function App() {
   return (
     <div>
       Hello World!
+      <br/>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas numquam ex quis suscipit vero odit minima molestiae, optio ducimus accusamus dignissimos. Officia nostrum alias quae quibusdam porro debitis possimus excepturi?
     </div>
   )
 }
